f12c3054e72cc863184723f094fff961402c185d
Kiselev Vlad <vladkaka005@gmail.com>
2023-04-08 13:49:16 +0700

    First commit

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,4 @@
 text = "hello word of word"
- 
+chars_popularity = {x:text.count(text[x]) for x in [x for x in (set(text.split()))]}
+words_popularity = {x:text.count(x) for x in [x for x in set(text.split())]}
+print(chars_popularity, words_popularity)
